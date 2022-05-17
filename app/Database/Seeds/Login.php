ea865bd1dada0ffa4c03417f4c12ac12aec2fe06
PedroAlbertini <pedro.fernandes56@etec.sp.gov.br>
2022-05-16 22:23:13 -0300

    criacao do seeder

---
+++ app/Database/Seeds/Login.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class Login extends Seeder
+{
+    public function run()
+    {
+        //
+    }
+}
---
+++ app/Database/Seeds/Produtos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class Produtos extends Seeder
+{
+    public function run()
+    {
+        //
+    }
+}
