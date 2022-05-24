0d562af2d41e1ccc2bea28f89ca7706f391c40b4
PedroAlbertini <pedro.fernandes56@etec.sp.gov.br>
2022-05-23 21:48:39 -0300

    add view

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,6 +7,11 @@ class Home extends BaseController
 {
     public function index()
     {
+        $this->load->view('html_header');
+        $this->load->view('conteudo');
+        $this->load->view('menu_categorias');
+        $this->load->view('rodape');
+
 
         $dados=$this->request->getPost();
         $model = new loginModels();
